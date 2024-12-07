66c6fc3b530c8d6b648e723ce9c671cb95707ca7
adrixo <adrianvalrom@gmail.com>
2024-12-07 00:45:20 +0000

    feat: update base

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/puntua-tu-mesa-demo/',
   plugins: [
     VueRouter(),
     Vue({
